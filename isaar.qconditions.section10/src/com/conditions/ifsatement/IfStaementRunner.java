30ac99bc9172901577cf7d29ae805509ed07f2ec
mohammed isaar khan <mohammed isaar khan@10.0.0.38>
2020-06-23 19:34:57 -0500

    section 10

---
+++ isaar.qconditions.section10/src/com/conditions/ifsatement/IfStaementRunner.java
@@ -0,0 +1,29 @@
+package com.conditions.ifsatement;
+
+public class IfStaementRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int i=24;
+			//i is 24
+			//i is 25
+			//i is neither 24 or 25
+			
+			if (i ==25) {
+				System.out.println("i=25");
+				
+			}
+			else if (i == 24){
+				System.out.println("i=24");
+				}
+			else if (i ==23) {
+				System.out.println("i=23");
+				}
+			else {
+				System.out.println("i !=24 and i !=25 and i !=23" );
+				
+			}
+			
+			}
+
+}
---
+++ isaar.qconditions.section10/src/com/conditions/ifsatement2/MenuRunner.java
@@ -0,0 +1,18 @@
+package com.conditions.ifsatement2;
+
+import java.util.Scanner;
+
+public class MenuRunner {
+
+	public static void main(String[] args) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter Number1:");
+			int number1 = scanner.nextInt();
+			System.out.println("the number you entered is -" + number1);
+			
+			
+			
+			
+	}
+
+}
